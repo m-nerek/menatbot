d2e2e942ffd983cb0fe640c92abb954a4e622eaa
tech <technicaltysfv@gmail.com>
2020-07-31 12:22:44 +0100

    la la la bug

--- sosplay.py
+++ sosplay.py
@@ -7,14 +7,14 @@ def respond(message):
 
 	#if "CFTony" in message.author.name:
 	#	searchTerm="rickroll"
-	play(searchTerm)
+	return play(searchTerm)
 
 def play(searchTerm):
 	for i in range(5):
 		results = YoutubeSearch(searchTerm, max_results=1)
 		if len(results.videos)>0:
-			return "https://www.youtube.com"+results.videos[0]["url_suffix"]
+			return f"https://www.youtube.com{results.videos[0]['url_suffix']}"
 	return ":guile: I can't play that Tony"
 
 
-#print(play("despacito"))
+#print(play("music for morning people"))
