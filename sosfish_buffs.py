c83e02739672f56d98445ee3e3f9e21863c9d589
tech <technicaltysfv@gmail.com>
2020-09-06 00:26:17 +0100

    testing

--- sosfish_buffs.py
+++ sosfish_buffs.py
@@ -156,7 +156,7 @@ def campfire_main_loop(name, location, parameters, data):
 	if location == "":
 		location = data[name]["currentlocation"]
 
-	#print(data[location]["campfire"]["stewscore"])
+	print(data[location])
 	if data[location]["campfire"]["timer"] == 0:
 		data[location]["campfire"]["timer"] = "0"
 
